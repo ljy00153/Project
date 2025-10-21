56da45b739cacf64d38e90bdd538c197ce39f758
ljy00153 <a1820082004@gmail.com>
2025-10-21 11:12:49 +0800

    better organization

--- testbench/GEMM_base.hpp
+++ testbench/GEMM_base.hpp
@@ -2,12 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include "../src/PE/pe_array.cpp"
-#include "Load_data.hpp"
+
 #include "../analayzer/mapper_base.hpp"
 
-
 using namespace std;
+using DataType = int32_t;
+void load_data(std::vector<DataType> &mem, const std::string &filename);
 
 class GEMM_base  
 {
@@ -160,3 +164,32 @@ class GEMM_base
                                     const std::vector<DataType>& all_weights,
                                     std::vector<DataType>& final_psums) = 0;
 };
+
+void load_data(std::vector<DataType> &mem, const std::string &filename)
+{
+    ifstream file(filename);
+    if (!file.is_open()) 
+    {
+        cerr << "   Error opening file: " << filename << endl;
+        return;
+    }
+    else
+        cout << "   Successfully open file: " << filename << endl;
+    string line;
+    while (getline(file, line)) 
+    {
+        if (line.empty()) continue;
+        int32_t val;
+        stringstream ss(line);
+        ss >> hex >> val;
+        if (ss.fail()) 
+        {
+            cerr << "⚠️  Invalid line in " << filename << ": " << line << endl;
+            continue;
+        }
+        //cout << "load value: " << val << endl;
+        mem.push_back(val);
+    }
+
+    file.close();
+}
